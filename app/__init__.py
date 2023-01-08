1392d1b5b7db3b3790a3248a176ef2013ca94fd9
DLiaw <DL0429922@gmail.com>
2023-01-07 21:47:44 -0500

    backend routes for images

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .api.review_routes import review_routes
 from .api.business_routes import business_routes
+from .api.business_images.py import business_images_routes
 from .config import Config
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
@@ -32,6 +33,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(review_routes, url_prefix='/api/reviews')
 app.register_blueprint(business_routes, url_prefix='/api/business')
+app.register_blueprint(business_images_routes, url_prefix='/api/business')
 db.init_app(app)
 Migrate(app, db)
 
---
+++ app/api/business_images.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, Blueprint, redirect, request
+from ..models import db, User, Review, Business, ReviewImage, BusinessImage
+from ..forms import BusinessForm, BusinessImageForm, ReviewForm, ReviewImageForm, SignUpForm, LoginForm
+from flask_login import login_required, current_user
+from app.api.auth_routes import validation_errors_to_error_messages
+business_routes = Blueprint("business_iamges", __name__)
+
+## Adding an image for business
+
+@business_images_routes.route('/new',methods=['POST'])
+@login_required
+def business_image():
+    form = BusinessImageForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_business_image = BusinessImage(
+            business_image = form.data['business_image']
+        )
+        db.session.add(new_business_image)
+        db.session.commit()
+        return new_business_image.to_dict()
+    return {"errors": form.errors}, 401
+
+## Delete image for business
+
+@business_images_routes.route('/<int:id>/edit',methods=['PUT'])
+@login_required
+def update_business_image(id):
+    updated_business_image = BusinessImage.query.filter_by(id = id).first()
+    form = BusinessImageForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        setattr(updated_business_image, 'business_image' = form.data['business_iamge'])
+         db.session.commit()
+        res = updated_business_image.to_dict()
+        return res
+    if form.errors:
+        return {"errors": form.errors}, 401
