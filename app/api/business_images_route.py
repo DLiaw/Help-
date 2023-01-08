ba0def21589b03ce2e6b708e90a5007900dbb832
DLiaw <DL0429922@gmail.com>
2023-01-07 20:22:56 -0500

    fix render issues

--- app/api/business_images_route.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, jsonify, Blueprint, redirect, request
-from ..models import db, User, Review, Business, ReviewImage, BusinessImage
-from ..forms import BusinessForm, BusinessImageForm, ReviewForm, ReviewImageForm, SignUpForm, LoginForm
-from flask_login import login_required, current_user
-from .auth_routes import validation_errors_to_error_messages
-review_routes = Blueprint("images", __name__)
